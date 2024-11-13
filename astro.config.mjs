05310b9991934ab2b6061bb80609d80678184deb
Tyler Limbach <tylimbach@gmail.com>
2024-11-12 19:48:45 -0600

    tweaking github workflow build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tylimbach.github.io/blog/',
+  outDir: 'dist',
   integrations: [mdx(), sitemap()],
 });
