5c23e96267ecc3e128d64903bd6f8fb791d10001
Tyler Limbach <tylimbach@gmail.com>
2024-11-12 19:42:23 -0600

    test deploy target change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tylimbach.github.io/blog/',
-  outDir: 'dist',
   integrations: [mdx(), sitemap()],
 });
