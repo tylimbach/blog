fb6d7c5e292d8c9c43af917833ec7bf837d550f5
Tyler Limbach <tylimbach@gmail.com>
2024-11-13 20:45:10 -0600

    update home, about, align header center

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tyler Limbach';
-export const SITE_DESCRIPTION = 'Thoughts and journeys of a young software engineer';
+export const SITE_TITLE = 'Immature Optimization';
+export const SITE_DESCRIPTION = 'A young engineer\'s thoughts and journeys into optimization';
